40d99b1febc7f9d159416297e8554cb41290dfa6
Ketul Makwana <ketulmakwana09@gmail.com>
2020-01-09 21:38:13 +0530

    Add a table row to show entered book details

--- app.js
+++ app.js
@@ -26,6 +26,21 @@ class UI {
         const books = StoredBooks;
         books.forEach((book) => UI.addBooktoList(book));
     }
+
+    static addBooktoList(book){
+        const list = document.querySelector('#book-list');
+    
+        const row = document.createElement('tr');
+    
+        row.innerHTML = `
+                    <td>${book.title}</td>
+                    <td>${book.author}</td>
+                    <td>${book.isbn}</td>
+                    <td><a href="#" class='btn btn-danger btn-sm delete'>X</a></td>
+                    `;
+        
+        list.appendChild(row);
+    }
 }
 // Store Class: Handles Storage
 
