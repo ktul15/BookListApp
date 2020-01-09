a4e98e879a9db0abc672c38eb89056edfaf005d8
Ketul Makwana <ketulmakwana09@gmail.com>
2020-01-09 20:23:52 +0530

    Add book class to represent a book

---
+++ app.js
@@ -0,0 +1,18 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, isbn){
+        this.title = title,
+        this.author = author,
+        this.isbn = isbn
+    }
+}
+
+// UI Class: Handle UI tasks
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+
+// Event: Add a book
+
+// Event: Remove a book
